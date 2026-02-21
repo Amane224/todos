b295ffd62a252a68138688494f416f7d53d891a8
Koruchi <48584679-Koruchi@users.noreply.replit.com>
2026-02-21 12:30:03 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let nilai = 100
+
+if (nilai === 90) {
+  console.log("a")
+} else if (nilai === 80) {
+  console.log("b")
+} else if (nilai === 70) {
+  console.log("c")
+} else {
+  console.log("tidak lulus")
+}
+
+console.log(nilai)
